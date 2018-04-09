40ac4ee7077c98eeaca840edb75393b364b53ea0
Jacob Burton <jacobburton@gmail.com>
2018-04-09 12:25:05 -0600

    completed mini project

---
+++ src/ducks/counter.js
@@ -0,0 +1,81 @@
+const initialState = 
+{
+    currentValue: 0,
+    previousValues: [],
+    futureValues: []
+}   
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const UNDO = 'UNDO';
+const REDO = 'REDO';
+
+export function increment(num)
+{
+    return (
+    {
+        type: INCREMENT,
+        payload: num
+    });
+}
+
+export function decrement(num)
+{
+    return (
+    {
+        type: DECREMENT,
+        payload: num
+    });
+}
+
+export function undo()
+{
+    return (
+    {  
+        type: UNDO
+    });
+}
+
+export function redo()
+{
+    return (
+    {
+        type: REDO
+    });
+}
+
+export default function reducer (state = initialState, action)
+{
+    switch (action.type)
+    {
+        case INCREMENT:
+            return ({
+                currentValue: state.currentValue + action.payload,
+                futureValues: [],
+                previousValues: [state.currentValue, ...state.previousValues]
+            });
+        case DECREMENT:
+            return ({
+                currentValue: state.currentValue - action.payload,
+                futureValues: [],
+                previousValues: [state.currentValue, ...state.previousValues]
+            });
+        case UNDO:
+            return (
+            {
+                currentValue: state.previousValues[0],
+                futureValues: [state.currentValue, ...state.futureValues],
+                previousValues: state.previousValues.slice(1, state.previousValues.length)
+            });
+        case REDO: 
+            return (
+            {    
+                currentValue: state.futureValues[0],
+                futureValues: state.futureValues.slice(1, state.futureValues.length),
+                previousValues: [state.currentValue, ...state.previousValues]
+            });
+        default:
+            return state;
+    }
+}
+
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import reducer from './ducks/counter';
+
+export default createStore(reducer);
+
+
+
+
+
+
+
